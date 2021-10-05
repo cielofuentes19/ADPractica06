9fb44ef714443bc95cc6c86e9734db1d0c9111fb
AREMI <iremapmedina@gmail.com>
2021-10-04 21:26:54 -0500

    Resolución de la experiencia 01 de la practica de laboratorio 06

---
+++ Creacion de una Base de Datos.sql
@@ -0,0 +1,15 @@
+INSERT INTO CLIENTES VALUES(1, 'Juan Perez', 'Los Portales 310', '959663322', 'jperez@hotmail.com',30); 
+INSERT INTO CLIENTES VALUES(2, 'José Díaz', 'Los Angeles de Cayma 250', '959808080', 'jdiaz@hotmail.com',35);
+INSERT INTO CLIENTES VALUES(3, 'Roberto Gonzales', 'Las Orquideas 500', '959757878', 'rgonzales@hotmail.com',38);
+INSERT INTO CLIENTES VALUES(4, 'Ruben Peralta', 'Los Portales 510', '959663322', 'rperalta@hotmail.com',30);
+INSERT INTO CLIENTES VALUES(5, 'María de los Angeles', 'Juan Pablo V. y Guzman L-5', '959272754', 'angeles@hotmail.com',30);
+INSERT INTO CLIENTES VALUES(6, 'Arturo Ordoñez', 'Los Jasmines 250', '959707074', 'aordonez@hotmail.com',32);
+INSERT INTO CLIENTES VALUES(7, 'Josefa Lopez', 'Los Portales 10', '959202023', 'jlopez@hotmail.com',25);
+INSERT INTO CLIENTES VALUES(8, 'Sandra Ramirez', 'Palacio Viejo 100', '958757575', 'sramirez@hotmail.com',28);
+INSERT INTO CLIENTES VALUES(9, 'Pedro Lopez', 'Consuelo 440', '959695656', 'plopez@hotmail.com',30);
+INSERT INTO CLIENTES VALUES(10, 'Juanita Ortega', 'Palacio Viejo 250', '959909088', 'jortega@hotmail.com',30); 
+INSERT INTO CLIENTES VALUES(11, 'Marcia Sandoval', 'Pancho Fierro 250', '255000', 'plopez@hotmail.com',45); 
+INSERT INTO CLIENTES VALUES(12, 'Manuel Vega', 'Quinta Tristan 520', '425080', 'mvega@hotmail.com',42); 
+INSERT INTO CLIENTES VALUES(13, 'Ricardo Salas', 'Coop. Universitaria B-1', '959656522', 'rsalas@hotmail.com',27);
+
+SELECT * FROM CLIENTES;
